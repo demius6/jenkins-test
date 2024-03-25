f377a2c55e01129e1a6d02421eadc119051a59c7
demius6 <ya@gmail.com>
2024-03-25 21:09:45 +0300

    simple test

--- src/test/java/tests/PositiveTests.java
+++ src/test/java/tests/PositiveTests.java
@@ -1,2 +1,31 @@
-package tests;public class PositiveTests {
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+
+    @Test
+    void test00() {
+        assertTrue(true);
+    }    @Test
+    void test01() {
+        assertTrue(true);
+    }    @Test
+    void test02() {
+        assertTrue(true);
+    }    @Test
+    void test03() {
+        assertTrue(true);
+    }    @Test
+    void test04() {
+        assertTrue(true);
+    }    @Test
+    void test05() {
+        assertTrue(true);
+    }    @Test
+    void test06() {
+        assertTrue(true);
+    }
 }
